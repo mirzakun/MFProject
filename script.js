037302025b480a3e290e204db194f17e85b3d7a0
Mirza <84970315+mirzakun@users.noreply.github.com>
2022-01-24 11:01:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// selector
+const big_title = document.querySelector(".big-title");
+const header = document.querySelector("header");
+const shadow = document.querySelector(".shadow");
+const content = document.querySelector(".content");
+const section = document.querySelector("section");
+const image_container = document.querySelector(".imgContainer");
+const opacity = document.querySelectorAll(".opacity");
+const border = document.querySelector(".border");
+// end selector
+
+let header_height = header.offsetHeight;
+let section_height = section.offsetHeight;
+
+window.addEventListener('scroll', () => {
+    let scroll = window.pageYOffset;
+    let sectionY = section.getBoundingClientRect();
+
+    opacity.forEach(element => {
+        element.style.opacity = scroll / (sectionY.top + section_height);
+    })
+
+    big_title.style.opacity = - scroll / (header_height / 2) + 1;
+    shadow.style.height = `${scroll * 0.5 + 300}px`;
+
+    content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
+    image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
+
+    border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
+});
+
+var indexValue = 0;
+function slideShow() {
+    setTimeout(slideShow, 70000);
+    var x;
+    const img = document.querySelectorAll('.images img')
+    for(x = 0; x < img.length; x++){
+        img[x].style.display = "none"
+    }
+    indexValue++;
+    if(indexValue > img.length){indexValue = 1}
+    img[indexValue -1].style.display = "block"
+}slideShow();
